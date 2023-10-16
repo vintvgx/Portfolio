8463d16498c92f6269cf42d065271317dfdbb22c
bandit.studios <banditstudiosdpd@gmail.com>
2023-10-16 11:22:21 -0400

    Update Header icon colors + ProjectView resizing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,9 +9,10 @@ import { useState } from "react";
 
 type HeaderProps = {
   className?: string;
+  backgroundColor?: string;
 };
 
-const Header: React.FC<HeaderProps> = ({ className }) => {
+const Header: React.FC<HeaderProps> = ({ className, backgroundColor }) => {
   const location = useLocation();
   const isActive = (path: string) => location.pathname === path;
 
@@ -34,6 +35,8 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
     backgroundColor: "transparent",
     color: "black",
   };
+
+  const iconColor = backgroundColor === "white" ? "black" : "white";
 
   return (
     <div className={`header ${className}`}>
@@ -62,13 +65,13 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
       <div
         className={`menu-icon ${isMobileMenuOpen ? "open" : ""}`}
         onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-        <FaBars color={"black"} />
+        <FaBars color={iconColor} />
       </div>
       <div className={`social-icons ${isMobileMenuOpen ? "open" : ""}`}>
-        <AiFillMail className="icon" />
-        <FaGithub className="icon" />
-        <FaLinkedin className="icon" />
-        <FaTwitter className="icon" />
+        <AiFillMail className="icon" color={iconColor} />
+        <FaGithub className="icon" color={iconColor} />
+        <FaLinkedin className="icon" color={iconColor} />
+        <FaTwitter className="icon" color={iconColor} />
       </div>
     </div>
   );
