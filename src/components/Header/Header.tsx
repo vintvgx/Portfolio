4ffd4d8e2894a060a04e0c2fcdbceee483f42883
bandit.studios <banditstudiosdpd@gmail.com>
2023-10-26 11:52:28 -0400

    Updated links modal for mobile view

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,8 @@
-import * as React from "react";
+import React, { useState } from "react";
+import { AiFillMail } from "react-icons/ai";
+import { FaBars, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { NavLink, useLocation } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { AiFillMail } from "react-icons/ai"; // For the Gmail icon
-
 import "./Header.css";
-import { useState } from "react";
 
 type HeaderProps = {
   className?: string;
@@ -15,17 +12,12 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ className, backgroundColor }) => {
   const location = useLocation();
   const isActive = (path: string) => location.pathname === path;
-
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
 
   const activeNavStyle = {
-    backgroundColor: "white",
     color: "black",
-    // padding: "15px 20px",
     borderRadius: "40px",
-    boxShadow: "rgba(0, 0, 0, 0.1) 0px 8px 16px 0px",
-    width: "90px",
-    height: "40px",
     display: "flex",
     alignItems: "center",
     placeContent: "center",
@@ -33,7 +25,16 @@ const Header: React.FC<HeaderProps> = ({ className, backgroundColor }) => {
 
   const defaultNavStyle = {
     backgroundColor: "transparent",
-    color: "black",
+    color: "rgba(145, 145, 145, 0.856)",
+  };
+
+  const openModal = () => {
+    setIsMobileMenuOpen(false); // Close mobile menu if open
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -50,7 +51,7 @@ const Header: React.FC<HeaderProps> = ({ className, backgroundColor }) => {
           to="/Projects"
           style={isActive("/Projects") ? activeNavStyle : defaultNavStyle}
           className="nav-link">
-          <p>Projects</p>
+          <p>Work</p>
         </NavLink>
         <NavLink
           to="/About"
@@ -58,18 +59,88 @@ const Header: React.FC<HeaderProps> = ({ className, backgroundColor }) => {
           className="nav-link">
           <p>About</p>
         </NavLink>
+
+        <div
+          className={`menu-icon ${isMobileMenuOpen ? "open" : ""}`}
+          onClick={openModal}>
+          <FaBars
+            style={{
+              color: "rgba(145, 145, 145, 0.856)",
+              marginLeft: "10px",
+              marginTop: "2px",
+            }}
+            color={"black"}
+          />
+        </div>
       </div>
-      <div
-        className={`menu-icon ${isMobileMenuOpen ? "open" : ""}`}
-        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-        <FaBars color={"black"} />
-      </div>
-      <div className={`social-icons ${isMobileMenuOpen ? "open" : ""}`}>
-        <AiFillMail className="icon" color={"black"} />
-        <FaGithub className="icon" color={"black"} />
-        <FaLinkedin className="icon" color={"black"} />
-        <FaTwitter className="icon" color={"black"} />
-      </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <a className="close-modal" onClick={closeModal}>
+              &times;
+            </a>
+            {/* <div className="social-icons">
+              <AiFillMail className="icon" color={"black"} />
+              <a
+                href="https://github.com/vintvgx?tab=overview&from=2023-10-01&to=2023-10-25"
+                target="_blank"
+                rel="noreferrer noopener">
+                <FaGithub className="icon" size={16} />
+                <h1>Github</h1>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kareem-saygbe-63b82a1b4/"
+                target="_blank"
+                rel="noreferrer noopener">
+                <FaLinkedin className="icon" size={16} />
+              </a>
+              <a
+                href="https://x.com/devplusdesign?s=20"
+                target="_blank"
+                rel="noreferrer noopener">
+                <FaTwitter className="icon" size={16} />
+              </a>
+            </div> */}
+            <div
+              className="social-icons"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                gap: "20px",
+              }}>
+              <a
+                href="https://github.com/vintvgx?tab=overview&from=2023-10-01&to=2023-10-25"
+                target="_blank"
+                rel="noreferrer noopener">
+                {/* <FaGithub className="icon" size={16} /> */}
+                <span className="icon">Github</span>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kareem-saygbe-63b82a1b4/"
+                target="_blank"
+                rel="noreferrer noopener">
+                {/* <FaGithub className="icon" size={16} /> */}
+                <span className="icon">LinkedIN</span>
+              </a>
+              <a
+                href="https://x.com/devplusdesign?s=20"
+                target="_blank"
+                rel="noreferrer noopener">
+                {/* <FaGithub className="icon" size={16} /> */}
+                <span className="icon">Twitter</span>
+              </a>
+              <a href="/RESUME.pdf" target="_blank" rel="noreferrer noopener">
+                {/* <FaGithub className="icon" size={16} /> */}
+                <span className="icon">Resume</span>
+              </a>
+            </div>
+            <div className="lets-talk-button">Let's Talk</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
