63cbecb63b57cbc60fc396710418d39812b9cade
bandit.studios <banditstudiosdpd@gmail.com>
2024-01-09 15:15:13 -0500

    Updated code for google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home/Home";
 import Terminal from "./views/Terminal/Terminal";
 import ReactGA from "react-ga";
 
 ReactGA.initialize("G-DH38G3QNMP");
-ReactGA.pageview(window.location.pathname + window.location.search);
+
+const TrackPageView = () => {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+  return null;
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <TrackPageView /> {/* Call TrackPageView to track page views */}
         <Routes>
           <Route path="/" element={<Terminal />} />
           <Route path="/*" element={<Home />} />
