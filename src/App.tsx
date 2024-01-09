4e25e3579eec0283daca1615bd4ed4dd23527374
bandit.studios <banditstudiosdpd@gmail.com>
2024-01-09 15:14:32 -0500

    Adding google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home/Home";
 import Terminal from "./views/Terminal/Terminal";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-DH38G3QNMP");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App: React.FC = () => {
   return (
